c9443c851c27d20f5dbb3e0027b40eff8f3dd3a7
Thomas Rieschl <rieschl@users.noreply.github.com>
2020-05-22 21:33:54 +0200

    implement caching headers and -response (#4)
    * implement caching headers and -response
    * update code quality tools

--- src/Service/AssetManager.php
+++ src/Service/AssetManager.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace Eventjet\AssetManager\Service;
 
+use DateTimeZone;
 use Eventjet\AssetManager\Resolver\ResolverInterface;
+use Fig\Http\Message\StatusCodeInterface;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use RuntimeException;
+use Safe\DateTimeImmutable;
+use Safe\Exceptions\DatetimeException;
+
+use function gmdate;
+use function is_string;
+
+use const DATE_RFC7231;
 
 final class AssetManager
 {
@@ -27,7 +37,7 @@ final class AssetManager
         return $this->resolver->resolve($request->getUri()->getPath()) !== null;
     }
 
-    public function buildAssetResponse(RequestInterface $request): ResponseInterface
+    public function buildAssetResponse(ServerRequestInterface $request): ResponseInterface
     {
         $asset = $this->resolver->resolve($request->getUri()->getPath());
         if ($asset === null) {
@@ -35,8 +45,37 @@ final class AssetManager
                 'Asset could not be resolved. Use "resolvesToAsset" before "buildAssetResponse".'
             );
         }
-        return (new Response())
-            ->withStatus(200)
+        $lastModified = \Safe\filemtime($asset->getPath());
+        $etagFile = \Safe\md5_file($asset->getPath());
+
+        $serverParams = $request->getServerParams();
+        $ifModifiedSince = $serverParams['HTTP_IF_MODIFIED_SINCE'] ?? null;
+        $etagHeader = $serverParams['HTTP_IF_NONE_MATCH'] ?? null;
+
+        if (is_string($ifModifiedSince)) {
+            try {
+                $ifModifiedSince = DateTimeImmutable::createFromFormat(
+                    DATE_RFC7231,
+                    $ifModifiedSince,
+                    new DateTimeZone('UTC')
+                )->getTimestamp();
+            } catch (DatetimeException $exception) {
+                $ifModifiedSince = null;
+            }
+        }
+
+        $response = (new Response())
+            ->withAddedHeader('Last-Modified', gmdate(DATE_RFC7231, $lastModified))
+            ->withAddedHeader('Etag', $etagFile)
+            ->withAddedHeader('Cache-Control', 'public');
+
+        if ($etagHeader === $etagFile || $ifModifiedSince >= $lastModified) {
+            return $response
+                ->withStatus(StatusCodeInterface::STATUS_NOT_MODIFIED, 'Not Modified');
+        }
+
+        return $response
+            ->withStatus(StatusCodeInterface::STATUS_OK)
             ->withAddedHeader('Content-Transfer-Encoding', 'binary')
             ->withAddedHeader('Content-Type', $asset->getMimeType())
             ->withAddedHeader('Content-Length', $asset->getContentLength())
--- tests/unit/Service/AssetManagerTest.php
+++ tests/unit/Service/AssetManagerTest.php
@@ -8,9 +8,12 @@ use Eventjet\AssetManager\Asset\FileAsset;
 use Eventjet\AssetManager\Service\AssetManager;
 use Eventjet\Test\Unit\AssetManager\ObjectFactory;
 use Eventjet\Test\Unit\AssetManager\TestDouble\ResolverStub;
+use Fig\Http\Message\StatusCodeInterface;
 use Laminas\Diactoros\StreamFactory;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
+
+use function gmdate;
 
 class AssetManagerTest extends TestCase
 {
@@ -32,15 +35,101 @@ class AssetManagerTest extends TestCase
     public function testBuildAssetResponseReturnsSuccessResponse(): void
     {
         $asset = new FileAsset(ObjectFactory::tmpFile('/** js */', 'test.js'));
+
         $this->resolver->setResolvedAsset($asset);
 
         $response = $this->manager->buildAssetResponse(ObjectFactory::serverRequest());
 
+        $expectedLastModify = gmdate(DATE_RFC7231, \Safe\filemtime($asset->getPath()));
         self::assertSame(200, $response->getStatusCode());
-        self::assertSame('binary', $response->getHeaders()['Content-Transfer-Encoding'][0]);
-        self::assertSame('application/javascript', $response->getHeaders()['Content-Type'][0]);
-        self::assertSame('9', $response->getHeaders()['Content-Length'][0]);
+        self::assertSame('binary', $response->getHeaderLine('Content-Transfer-Encoding'));
+        self::assertSame('application/javascript', $response->getHeaderLine('Content-Type'));
+        self::assertSame('9', $response->getHeaderLine('Content-Length'));
+        self::assertSame($expectedLastModify, $response->getHeaderLine('Last-Modified'));
+        self::assertSame(\Safe\md5_file($asset->getPath()), $response->getHeaderLine('Etag'));
+        self::assertSame('public', $response->getHeaderLine('Cache-Control'));
         self::assertSame('/** js */', $response->getBody()->getContents());
+    }
+
+    public function testReturnsNotModifiedIfEtagMatches(): void
+    {
+        $asset = new FileAsset(ObjectFactory::tmpFile('/** js */', 'test.js'));
+        $this->resolver->setResolvedAsset($asset);
+        $request = ObjectFactory::serverRequest(
+            null,
+            null,
+            ['HTTP_IF_NONE_MATCH' => \Safe\md5_file($asset->getPath())]
+        );
+
+        $response = $this->manager->buildAssetResponse($request);
+
+        self::assertSame(StatusCodeInterface::STATUS_NOT_MODIFIED, $response->getStatusCode());
+    }
+
+    public function testReturnsNotModifiedIfModifiedSinceMatches(): void
+    {
+        $asset = new FileAsset(ObjectFactory::tmpFile('/** js */', 'test.js'));
+        $this->resolver->setResolvedAsset($asset);
+        $filemtime = \Safe\filemtime($asset->getPath());
+        $wantedLastModify = gmdate(DATE_RFC7231, $filemtime);
+        $request = ObjectFactory::serverRequest(
+            null,
+            null,
+            ['HTTP_IF_MODIFIED_SINCE' => $wantedLastModify]
+        );
+
+        $response = $this->manager->buildAssetResponse($request);
+
+        self::assertSame(StatusCodeInterface::STATUS_NOT_MODIFIED, $response->getStatusCode());
+    }
+
+    public function testReturnsCompleteAssetIfModifiedSinceIsOlderThanFile(): void
+    {
+        $asset = new FileAsset(ObjectFactory::tmpFile('/** js */', 'test.js'));
+        $this->resolver->setResolvedAsset($asset);
+        $filemtime = \Safe\filemtime($asset->getPath());
+        $wantedLastModify = gmdate(DATE_RFC7231, $filemtime - 86400);
+        $request = ObjectFactory::serverRequest(
+            null,
+            null,
+            ['HTTP_IF_MODIFIED_SINCE' => $wantedLastModify]
+        );
+
+        $response = $this->manager->buildAssetResponse($request);
+
+        self::assertSame(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
+    }
+
+    public function testReturnsNotModifiedIfModifiedSinceIsNewerThanFile(): void
+    {
+        $asset = new FileAsset(ObjectFactory::tmpFile('/** js */', 'test.js'));
+        $this->resolver->setResolvedAsset($asset);
+        $filemtime = \Safe\filemtime($asset->getPath());
+        $wantedLastModify = gmdate(DATE_RFC7231, $filemtime + 86400);
+        $request = ObjectFactory::serverRequest(
+            null,
+            null,
+            ['HTTP_IF_MODIFIED_SINCE' => $wantedLastModify]
+        );
+
+        $response = $this->manager->buildAssetResponse($request);
+
+        self::assertSame(StatusCodeInterface::STATUS_NOT_MODIFIED, $response->getStatusCode());
+    }
+
+    public function testInvalidIfNotModifiedSinceHeaderIsIgnored(): void
+    {
+        $asset = new FileAsset(ObjectFactory::tmpFile('/** js */', 'test.js'));
+        $this->resolver->setResolvedAsset($asset);
+        $request = ObjectFactory::serverRequest(
+            null,
+            null,
+            ['HTTP_IF_MODIFIED_SINCE' => 'foo']
+        );
+
+        $response = $this->manager->buildAssetResponse($request);
+
+        self::assertSame(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
     }
 
     protected function setUp(): void
